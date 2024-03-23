b11d8134d6fd27145182a1e0a9bb7a59dd68ec5d
Sauvikesh <saulal@ucdavis.edu>
2024-03-22 19:54:40 -0700

    finished

--- src/countdown-timer.tsx
+++ src/countdown-timer.tsx
@@ -1,3 +1,44 @@
-const CountdownTimer = () => <div>Add your timer here!</div>;
+import { useState, useEffect } from 'react';
 
-export default CountdownTimer;
+// function that formats our time
+const formatDate = (dateObject: Date) => {
+  const minutes = dateObject.getMinutes();
+  const seconds = dateObject.getSeconds();
+
+  // formatting to make it look nicer visually, taken from solution
+  const paddedSeconds = seconds.toString().padStart(2, '0');
+  return `${minutes}:${paddedSeconds}`;
+};
+
+export default function CountdownTimer() {
+  const fiveMinutes = new Date(0, 0, 0, 0, 5);
+  const [timeRemaining, setTimeRemaining] = useState(fiveMinutes);
+  const [isCountingDown, setIsCountingDown] = useState(false);
+
+  const countdown = () => {
+    setTimeRemaining((timeRemaining) => new Date(timeRemaining.getTime() - 1000));
+    };
+
+  useEffect(() => {
+
+    // have a useState variable to start and stop the timer
+    if (isCountingDown) {
+        const intervalId = setInterval(() => {
+            countdown();
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }
+  }, [isCountingDown]);
+
+
+  return (
+    <div>
+      <h1>{formatDate(timeRemaining)}</h1>
+      <div>
+        <button onClick= {() => {setIsCountingDown(true)}}>Start</button>
+        <button onClick= {() => {setIsCountingDown(false)}}>Stop</button>
+        <button onClick= {() => {setTimeRemaining(new Date(0, 0, 0, 0, 5))}}>Reset</button>
+      </div>
+    </div>
+  );
+}
